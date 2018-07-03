56ff17830d355532ca33de01c25546dba5c1f17e
TTIT\polowianiuks <sandrapolowianiuk@gmail.com>
2018-07-03 14:27:09 +0200

    no message

--- src/main/java/com/mycompany/project_p/model/Document.java
+++ src/main/java/com/mycompany/project_p/model/Document.java
@@ -3,7 +3,23 @@ package com.mycompany.project_p.model;
 public class Document {
     private Long documentId;
     private String documentName;
-    private String desc;
+    private String documentDescription;
     private Long creatorId;
     private String topic;
+
+    public Document(){}
+    public Document(String documentName, String documentDescription, Long creatorId,String topic){
+        this.creatorId=creatorId;
+        this.documentDescription=documentDescription;
+        this.documentName=documentName;
+        this.topic=topic;
+    }
+
+    public void setDocumentName(String documentName) {
+        this.documentName = documentName;
+    }
+
+    public String getDocumentName() {
+        return documentName;
+    }
 }
