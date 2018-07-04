1945f79f119d201f107fe753596a45a5854549bf
TTIT\polowianiuks <sandrapolowianiuk@gmail.com>
2018-07-04 10:29:59 +0200

    model assigment and user update

--- src/main/java/com/mycompany/project_p/model/Assigment.java
+++ src/main/java/com/mycompany/project_p/model/Assigment.java
@@ -1,13 +1,58 @@
 package com.mycompany.project_p.model;
 
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
+@Entity
+@Table(name="ASSIGMENT")
 public class Assigment {
+    @Column(name = "ASSIGMENT_ID", nullable = false)
     private Long assigmentId;
     @OneToOne
+    @JoinColumn(name="USER_ID")
     private User user;
-    @OneToOne
+
     private Role role;
     @OneToOne
+    @JoinColumn(name="PROJECT_ID")
     private Project project;
+
+    public Assigment(Long assigmentId, User user, Role role, Project project) {
+        this.assigmentId = assigmentId;
+        this.user = user;
+        this.role = role;
+        this.project = project;
+    }
+
+    public Long getAssigmentId() {
+        return assigmentId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setAssigmentId(Long assigmentId) {
+        this.assigmentId = assigmentId;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
 }
