de96d6f9dd7c18f048631aa95d9ac7d29abb66c7
TTIT\pytela <agnieszkapytel@gmail.com>
2018-07-05 10:48:25 +0200

    fix document create command

--- src/main/java/com/dreamteam/project/component/DocumentCommands.java
+++ src/main/java/com/dreamteam/project/component/DocumentCommands.java
@@ -35,8 +35,9 @@ public class DocumentCommands {
     private final ConfigurationClass configurationClass;
     private Map<String, List<String>> permissions = new HashMap<>();
 
-    @ShellMethod("Create document (documentName, description, creatorID, topic)")
-    public String createDocument(String documentName, String desc, Long creatorId, String topic) {
+    @ShellMethod("Create document (documentName, description, topic)")
+    public String createDocument(String documentName, String desc, String topic) {
+        Long creatorId = configurationClass.getUser().getUserId();
         Document document = new Document(null, documentName, desc, creatorId, topic);
         document = repo.save(document);
         return ("Document created succesfully: " + document);
